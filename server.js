13e271d4c742eb29e7a1f13150cc2fa2b9c3c6f5
George Mcalpine <georgemcalpine@Georges-iMac.local>
2022-02-28 15:37:11 -0500

    first db query

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL username
+        user: 'root',
+        //MySqul password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
 app.use((req,res) => {
